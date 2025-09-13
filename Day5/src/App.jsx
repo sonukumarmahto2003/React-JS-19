c65122bd310854bb1dbac610c92e2bd49e183845
sonukumar1S <sonukumarcs2003@gmail.com>
2025-09-13 20:08:04 +0530

    React JS 19

---
+++ Day5/src/App.jsx
@@ -0,0 +1,23 @@
+function App(){
+  return(
+    <div>
+      <h1>First Component</h1>
+      <Fruit />
+      <Colors />
+    </div>
+  )
+}
+
+function Fruit(){
+  return(
+    <h1>Apple</h1>
+  )
+}
+
+function Colors(){
+  return(
+    <h1>Red Color</h1>
+  )
+}
+
+export default App;
---
+++ Day6/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Importing and Exporting Components</h1>
+    </div>
+  )
+}
+
+
+export default App;
